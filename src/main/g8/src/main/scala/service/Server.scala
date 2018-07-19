aaa5fdac1fc854b11f289609e5166edd0fd95b92
Siyavash Habashi <ghashange@live.com>
2018-07-19 23:01:13 +0200

    remove config singleton.

--- src/main/g8/src/main/scala/service/Server.scala
+++ src/main/g8/src/main/scala/service/Server.scala
@@ -3,22 +3,24 @@ package service
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
+import com.typesafe.config.ConfigFactory
 import com.typesafe.scalalogging.LazyLogging
-import config.Configs
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.util.{Failure, Success}
 
 object Server extends LazyLogging {
   def main(args: Array[String]): Unit = {
-    logger.info("Starting $project_name$ Service")
+    logger.info("Starting $project_name$ service.")
+
+    val config = ConfigFactory.load()
 
     implicit val system: ActorSystem = ActorSystem("$project_name$")
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
     implicit val materializer: ActorMaterializer = ActorMaterializer.create(system)
 
     val host = "0.0.0.0"
-    val port = Configs.port
+    val port = config.getInt("port")
 
     Http().bindAndHandle(Routes.routes, host, port)
       .onComplete {
