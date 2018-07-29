b19248ce8fa489fee76e68d2acb8acccffa90e50
Siyavash Habashi <ghashange@live.com>
2018-07-29 23:28:12 +0200

    refine helloWorldRoute path.

--- src/main/g8/src/main/scala/service/helloworld/HelloWorldRoute.scala
+++ src/main/g8/src/main/scala/service/helloworld/HelloWorldRoute.scala
@@ -4,8 +4,10 @@ import akka.http.scaladsl.server.{Directives, Route}
 
 class HelloWorldRoute extends Directives {
   val route: Route = {
-    pathPrefix("hello-world") {
-      complete("Hello World!")
+    path("hello-world") {
+      get {
+        complete("Hello World!")
+      }
     }
   }
 }
